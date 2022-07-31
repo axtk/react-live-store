22d44c1c3667bd600e40876a250cc04c415f4d8f
axtk <axtkx@inbox.ru>
2022-08-01 01:42:01 +0300

    add explicit return type to useLiveStore

--- index.ts
+++ index.ts
@@ -1,15 +1,18 @@
 import {useState, useEffect} from 'react';
 import {Observable, Observer, ObserverOptions} from 'object-observer';
 
+// the resolved type of what would be `ReturnType<typeof Observable.from<T>>`
+export type LiveObject<T extends object> = Observable & T;
+
 export type UpdateHandler = Observer;
 export type UpdateHandlerOptions = ObserverOptions;
 
 const isObject = <T = unknown>(value: T): value is NonNullable<T> =>
     value !== null && typeof value === 'object';
 
-export class LiveStore {
-    value: any;
-    constructor(value: any) {
+export class LiveStore<T extends object = {}> {
+    value: LiveObject<T>;
+    constructor(value: T) {
         if (!isObject(value))
             throw new Error('LiveStore value should be an object');
         this.value = Observable.from(value, {async: true});
@@ -28,7 +31,10 @@ export class LiveStore {
     }
 }
 
-export function useLiveStore(liveStore: LiveStore, options?: UpdateHandlerOptions) {
+export function useLiveStore<T extends object>(
+    liveStore: LiveStore<T>,
+    options?: UpdateHandlerOptions,
+): LiveObject<T> {
     let [, setRevision] = useState(0);
 
     if (!(liveStore instanceof LiveStore))
